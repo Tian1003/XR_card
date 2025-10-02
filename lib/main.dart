b91957ce222441dd221e414f8e1dceb122661ee0
SaiSan23 <b0979527778@gmail.com>
2025-10-02 16:01:50 +0800

    feat(connect): 新增QR碼掃描功能並優化連線頁面
    - 新增 mobile_scanner 依賴 QR 碼掃描
    - 在連線頁面新增掃描功能，支援透過 QR 碼新增聯絡人
    - 優化 UI 佈局，新增分割操作按鈕元件
    - 在 SupabaseService 中新增根據 QR token 取得使用者資訊的方法

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,22 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/home_page.dart';
 import 'data/supabase_services.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
+    // 天
     url: 'https://afqtdcbyezgjnearxgxz.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcXRkY2J5ZXpnam5lYXJ4Z3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyMzg4NTYsImV4cCI6MjA3MDgxNDg1Nn0.1YwgDMEBrss177ADUD1VvMcKqWU0skMmzubmq0UhTec',
+    // 蔡
+    // url: 'https://sjfjxhagyodvibvajmdm.supabase.co',
+    // anonKey:
+    //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZmp4aGFneW9kdmlidmFqbWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MDc2NjAsImV4cCI6MjA3MTE4MzY2MH0.9GqgOZT79mPJ2obPQ_uIcCU7xHNeb_BYYAaFD7qzrq4',
   );
-   
+
   // 建立並偵測裝置，iPad=1、iPhone=2
-  await SupabaseService.init(Supabase.instance.client); 
+  await SupabaseService.init(Supabase.instance.client);
 
   runApp(const MyApp());
 }
